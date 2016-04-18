12d3142dcd0bfc73c1cc839bb5793445ba135707
Doctor邓 <Doctor@10.216.130.122>
2016-04-18 16:09:52 +0800

    练习package和import语句的使用

---
+++ src/SunRise.java
@@ -0,0 +1,16 @@
+import tom.jiafei.Trangle;
+import java.util.Date;
+
+public class SunRise {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+        Trangle trangle = new Trangle(12,23,23);
+        trangle.getArea();
+        trangle.changeSide(22, 1, 1000);
+        trangle.getArea();
+        Date date = new Date();
+        System.out.println(date);
+	}
+
+}
