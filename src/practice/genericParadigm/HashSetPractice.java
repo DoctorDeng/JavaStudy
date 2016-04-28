285bbf9f125798e18e8a95d8f26329cb3c53cbc9
Administrator <Administrator@F504-69>
2016-04-27 18:13:20 -0700

    小的改动

---
+++ src/practice/genericParadigm/HashSetPractice.java
@@ -0,0 +1,61 @@
+package practice.genericParadigm;
+/*
+ * Description:   练习HashSet<E>泛型类
+ *         说明:   HashSet<E>泛型类在数据组织上类似数学上的集合，可以进行“交”、“并”、“差”等运算。
+ */
+
+import java.util.*;
+
+class StudentHash{
+	String name;
+	int score;
+	
+	StudentHash(String name ,int score){
+		this.name = name;
+		this.score = score;
+	}
+	
+}
+public class HashSetPractice {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+        StudentHash zs = new StudentHash("张三", 58),
+        		    ls = new StudentHash("李四", 99),
+        		    ww = new StudentHash("王五", 86);
+        
+        HashSet<StudentHash> stuHash = new HashSet<StudentHash>();
+        HashSet<StudentHash> stuSet = new HashSet<StudentHash>();
+        
+        stuHash.add(zs);
+        stuHash.add(ls);
+        stuHash.add(ww);
+        
+        stuSet.add(zs);
+        stuSet.add(ww);
+        
+        if(stuHash.contains(ls)){
+        	System.out.println("集合stuHash中有"+ls.name);
+        }
+        
+        if(stuHash.containsAll(stuSet)){
+        	System.out.println("集合stuHash中有集合stuSet");
+        }
+        
+        int number = stuSet.size();
+        System.out.println("stuSet集合包含有"+number+"个元素: ");
+        Object s[] = stuSet.toArray();
+        for(int i=0; i<number; i++){
+        	System.out.printf("姓名：%s, 分数：%d\n", ((StudentHash)s[i]).name, ((StudentHash)s[i]).score);
+        }
+        
+        number = stuHash.size();
+        System.out.println("stuHash中有"+ number+ "个元素： ");
+        Iterator<StudentHash> iter = stuHash.iterator();
+        while(iter.hasNext()){
+        	StudentHash stuTemp = iter.next();
+        	System.out.printf("姓名：%s, 分数：%d\n", stuTemp.name, stuTemp.score);
+        }
+	}
+
+}
---
+++ src/practice/genericParadigm/LikedListPractice.java
@@ -0,0 +1,61 @@
+package practice.genericParadigm;
+/*
+ * Description:  练习LinkedList<E>泛型类
+ */
+import java.util.*;
+
+class Student{
+	String name;
+	int score;
+	Student(String name, int score){
+		this.name = name;
+		this.score = score;
+	}
+}
+
+public class LikedListPractice {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		
+        LinkedList<Student> stuList = new LinkedList<Student>();
+        Student stu1 = new Student("张三",78),
+        		stu2 = new Student("李四",98),
+        		stu3 = new Student("王五",67);
+        
+        stuList.add(stu1);
+        stuList.add(stu2);
+        stuList.add(stu3);
+        
+        int number = stuList.size();    //size()返回链表的长度，即节点个个数。
+        System.out.println("现在链表中有"+number+"个节点");
+        
+        for(int i=0; i<number; i++){
+        	Student temp = stuList.get(i);//get()得到链表中指定位置处节点中的对象
+        	System.out.printf("第"+i+"节点中的数据, 学生：%s, 分数：%d\n", temp.name, temp.score);
+        }
+        
+        Student removeStu = stuList.remove(1);//remove()删除指定位置上的节点,并返回被删除节的的对象
+        System.out.printf("被删除的节点中的数据时:%s, %d\n", removeStu.name, removeStu.score);
+     
+        //set()将当前链表指定位置节点中的对象替换为指定对象，并返回被替换的对象
+        Student replaceStu = stuList.set(1,new Student("赵六",54));       
+        System.out.printf("被替换的节点的数据时:%s, %d\n", replaceStu.name, replaceStu.score);
+        
+        number = stuList.size();
+        System.out.println("现在链表中有"+number+"个节点");
+        for(int i=0; i< number; i++){
+        	Student temp = stuList.get(i);
+        	System.out.printf("第"+i+"个节点中的数据, 学生: %s, %d\n", temp.name, temp.score);
+        }
+        
+        if(stuList.contains(stu1)){ //判断链表节点中是否有节点含有指定对象
+        	System.out.println("链表包含"+stu1+": ");
+        	System.out.println(stu1.name+","+stu1.score);
+        }
+        else{
+        	System.out.println("链表没有节点含有"+stu1);
+        }
+	}
+
+}
