e10be98ee59a70fc944a75af68e3844e9be56c1f
Administrator <Administrator@F504-8>
2016-05-13 21:50:10 -0700

    练习

---
+++ src/practice/gui/Test01.java
@@ -0,0 +1,43 @@
+package practice.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Test01 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Test01 window = new Test01();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Test01() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
