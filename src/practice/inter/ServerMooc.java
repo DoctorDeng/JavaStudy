273b4f885b398916a92051b6e8ec09613d4060d5
Doctor邓 <Doctor@10.216.130.122>
2016-05-16 21:47:18 +0800

    练习

---
+++ src/practice/inter/ServerMooc.java
@@ -0,0 +1,59 @@
+package practice.inter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 
+ * @author Doctor邓
+ * 基于TCP协议的Socket通信，实现用户登录
+ * 服务器端
+ */
+public class ServerMooc {
+	public static void main(String[] args) {
+		Socket socket = null;
+		ServerSocket serverSocket = null;
+		int count = 0;
+		try {
+			//1.创建要给服务器端Socket，即ServerSocket，指定绑定端口，并监听此端口
+			serverSocket = new ServerSocket(7777);
+			
+			//2.调用accept()方法开始监听，等待服务端的连接
+			System.out.println("***服务器即将启动，等待客户端的连接***");
+			//循环监听,等待客户端的连接
+			while(true) {
+				//调用accept()方法开始监听，等待客户端的连接
+				socket = serverSocket.accept();
+				//创建一个新的线程
+				ServerThreadMooc serverThread = new ServerThreadMooc(socket); 
+				serverThread.start();
+				count ++;
+				System.out.println("客户端的数量: " + count);
+				InetAddress address = socket.getInetAddress();
+				System.out.println(address.getHostName());
+			}
+			//System.out.println("客户端: " + socket.getLocalAddress() + "已连接");
+			
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} finally {
+			
+				try {
+					if (serverSocket != null) {
+						serverSocket.close();
+					}
+				} catch (IOException e) {
+					// TODO 自动生成的 catch 块
+					e.printStackTrace();
+				}
+		}
+	}
+}
--- src/practice/thread/EnergySystem.java
+++ src/practice/thread/EnergySystem.java
@@ -4,6 +4,7 @@ public class EnergySystem {
 
 	//能量盒子,能量存储的地方
 	private final double[] energyBoxes;
+	private final Object lockObj = new Object();     //多线程的锁
 	
 	/**
 	 * 
@@ -20,19 +21,37 @@ public class EnergySystem {
 	
 	/**
 	 * 能量的转移，从一个盒子到另一个盒子
-	 * @param from 能量源
-	 * @param to   能量终点
+	 * @param from   能量源
+	 * @param to     能量终点
 	 * @param amount 能量值
 	 */
 	public void transfer (int from, int to, double amount) {
-		if (energyBoxes[from] < amount) {
-			return;
-		} 
-		System.out.print(Thread.currentThread().getName());
-		energyBoxes[from] -= amount;
-		System.out.printf("从%d转移%10.2f单位能量到%d", from, amount, to);
-		energyBoxes[to] += amount;
-		System.out.printf("能量总和:%10.2f%n",getTotalEnergies());
+		synchronized(lockObj) {
+//			if (energyBoxes[from] < amount) {
+//				return;
+//			} 
+			
+			//while循环，保证条件不满足时任务都会被条件阻挡
+			//而不是继续竞争CPU资源
+			while (energyBoxes[from] < amount) {
+				try {
+					//条件不满足时，将当前线程放入Wait Set中，Wait Set是属于锁对象的。
+					lockObj.wait();
+				} catch (InterruptedException e) {
+					// TODO 自动生成的 catch 块
+					e.printStackTrace();
+				}
+			}
+			System.out.print(Thread.currentThread().getName());
+			energyBoxes[from] -= amount;
+			System.out.printf("从%d转移%10.2f单位能量到%d", from, amount, to);
+			energyBoxes[to] += amount;
+			System.out.printf("能量总和:%10.2f%n",getTotalEnergies());
+			
+			//唤醒一个lockObj对象上等待的线程，这个不是在同一个线程，同一个操作中被执行的。
+			lockObj.notify();
+			
+		}
 	}
 	
 	/**
