3e553c7c02f0bf0c64b5cb4046c9eb2a968a17c1
Doctor邓 <Doctor@DESKTOP-6099SU7>
2016-07-26 18:18:52 +0800

    小的改动

--- src/javaReflectPractice/ReflectDemo2.java
+++ src/javaReflectPractice/ReflectDemo2.java
@@ -7,7 +7,7 @@ public class ReflectDemo2 {
 		 * new 创建对象 是静态加载类，在编译时刻就需要加载所有可能使用到的类
 		 * 通过动态加载类可以解决使用什么类加载什么类，而不是要加载全部类的问题。
 		 */
-		if ("Word".equals(args[0]))
+	/*	if ("Word".equals(args[0]))
 		{
 			Word w = new Word();
 			w.start();
@@ -15,7 +15,7 @@ public class ReflectDemo2 {
 		if ("Excel".equals(args[0])) {
 			Excel e = new Excel();
 			e.start();
-		}
+		}*/
 	}
 
 }
