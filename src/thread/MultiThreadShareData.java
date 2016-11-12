b84246a22a8ba3c36ee9f1a8f1b9ed62ffc5e168
Doctor邓 <Doctor@192.168.31.242>
2016-11-12 22:50:16 +0800

    java 多线程练习

--- src/thread/MultiThreadShareData.java
+++ src/thread/MultiThreadShareData.java
@@ -5,9 +5,23 @@ package thread;
  *
  */
 public class MultiThreadShareData {
-
 	public static void main(String[] args) {
-		
+		//通过此 Runnable 对象来实现对数据的操作
+		ShareData shareData = new ShareData();
+		new Thread(shareData).start();
+		new Thread(shareData){}.start();
 	}
-
+}
+class ShareData implements Runnable{
+	int j = 100;
+	public synchronized void increment() {
+		j++;
+	}
+	public synchronized void decrement() {
+		j--;
+	}
+	@Override
+	public void run() {
+		decrement();
+	}
 }
---
+++ src/thread/MultiThreadShareData1.java
@@ -0,0 +1,56 @@
+package thread;
+
+public class MultiThreadShareData1 {
+
+	public static void main(String[] args) {
+		ShareDataOne sharaData = new ShareDataOne();
+		/*	
+		new Thread(() -> {
+			sharaData.decrement();
+			sharaData.print();
+		}).start();
+		
+		new Thread(() -> {
+			sharaData.increment();
+			sharaData.print();
+		}).start();*/
+		
+		new Thread(new MyRunnableDecrement(sharaData)).start();
+		new Thread(new MyRunnableIncrement(sharaData)).start();
+		
+	}
+}
+class MyRunnableDecrement implements Runnable {
+	private ShareDataOne shareData;
+	public MyRunnableDecrement (ShareDataOne shareData) {
+		this.shareData = shareData;
+	}
+	@Override
+	public void run() {
+		shareData.decrement();
+	}
+}
+
+class MyRunnableIncrement implements Runnable {
+	private ShareDataOne shareData;
+	public MyRunnableIncrement (ShareDataOne shareData) {
+		this.shareData = shareData;
+	}
+	@Override
+	public void run() {
+		shareData.increment();
+	}
+}
+
+class ShareDataOne {
+	private int j = 100;
+	public synchronized void increment() {
+		j++;
+		print();
+	}
+	public synchronized void decrement() {
+		j--;
+		print();
+	}
+	public void print() {System.out.println(Thread.currentThread().getName() + "操作 J 后, J 的值是: " + j);}
+}
