97bdb10117a6203e70b3de5a2673c0df8b5326da
Administrator <Administrator@16101250-4-PC>
2016-11-16 15:43:29 +0800

    练习 java核心技术知识

---
+++ src/coreJavaPractice/TestBigNumber.java
@@ -0,0 +1,21 @@
+package coreJavaPractice;
+
+import java.math.BigInteger;
+
+/**
+ * 练习  Java  中 BigDecimal 和 BigInteger 的使用
+ * @author 邓华杰
+ *
+ */
+public class TestBigNumber {
+
+	public static void testBigInteger() {
+		//BigInteger.valueOf() 将普通数值转换为大数值
+		BigInteger bigInteger = BigInteger.valueOf(100);
+		bigInteger.a
+	}
+	
+	public static void main(String[] args) {
+		testBigInteger();
+	}
+}
---
+++ src/coreJavaPractice/TestLoop.java
@@ -0,0 +1,26 @@
+package coreJavaPractice;
+
+/**
+ * 练习 Java 中循环的跳出
+ * 
+ * @author 邓华杰
+ *
+ */
+public class TestLoop {
+
+	public static void testSkipLoop() {
+		ok: for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.println("i=" + i + ",j=" + j);
+				if (i == 5)
+					break ok;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		testSkipLoop();
+	}
+
+}
