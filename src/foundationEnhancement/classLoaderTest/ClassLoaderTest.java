66ffa5252466740e893497307beaa0e961a99723
DoctorDeng <1412145477@qq.com>
2016-11-29 22:51:52 +0800

    练习 类加载器

---
+++ src/foundationEnhancement/classLoaderTest/ClassLoaderTest.java
@@ -0,0 +1,33 @@
+package foundationEnhancement.classLoaderTest;
+
+/**
+ * Description: 练习 Java 类加载器
+ *
+ * @author Doctor邓
+ * @since 2016/11/29 22:28
+ */
+public class ClassLoaderTest {
+    public static void getClassLoader() {
+        System.out.println(ClassLoaderTest.class.getClassLoader().getClass().getName());
+        /**
+         * System 类的类加载器为空, 是因为 System 是由 Bootstrap 加载的
+         * 而 Bootstrap 是由 C++ 编写的随 JVM 内核启动的加载器
+         */
+        System.out.println(System.class.getClassLoader());
+    }
+
+    /**
+     * 按照层次结构打印加载 ClassLoaderTest 类的所有类加载器的名称
+     */
+    public static void printClassLoaderName() {
+        ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
+        while (classLoader != null) {
+            System.out.println("ClassLoader: " + classLoader.getClass().getName());
+            classLoader = classLoader.getParent();
+        }
+    }
+    public static void main(String[] args) {
+        printClassLoaderName();
+        getClassLoader();
+    }
+}
