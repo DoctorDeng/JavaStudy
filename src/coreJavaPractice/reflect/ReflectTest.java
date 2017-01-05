28bfa214717619f797b89857b5f2f79c76853a49
Administrator <Administrator@16101250-4-PC>
2017-01-05 18:56:49 +0800

    未完成的练习

---
+++ src/coreJavaPractice/reflect/ReflectTest.java
@@ -0,0 +1,21 @@
+package coreJavaPractice.reflect;
+
+import gatherFrame.Student;
+/**
+ * 练习泛型
+ * @author Administrator
+ *
+ */
+public class ReflectTest {
+
+	public static void FieldTest(){
+		Student stu = new Student("12", "邓华杰");
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
