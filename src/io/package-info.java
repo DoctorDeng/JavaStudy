8e0d17232ce0470b83d24c1e27040538062ee5ce
Administrator <Administrator@16101250-4-PC>
2018-01-24 18:25:15 +0800

    增加部分练习

---
+++ src/io/package-info.java
@@ -0,0 +1,29 @@
+/**  
+
+ * Copyright © 2018公司名字. All rights reserved.
+
+ *
+
+ * @Title: package-info.java
+
+ * @Prject: JavaStudy
+
+ * @Package: io
+
+ * @Description: TODO
+
+ * @author: denghuajie  
+
+ * @date: 2018年1月24日 下午6:14:13
+
+ * @version: V1.0  
+
+ */
+/** 
+* @ClassName: package-info 
+* @Description: TODO
+* @author denghuajie 
+* @date 2018年1月24日 下午6:14:13 
+*  
+*/
+package io;
