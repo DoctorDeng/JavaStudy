232ffe138fb3b35ddce8851f48a3376e067458d9
Administrator <Administrator@a.com>
2018-06-08 10:44:57 +0800

    删除蓝桥培训无用练习

--- src/system/rentCar/ICarGo.java
+++
@@ -1,7 +0,0 @@
-package system.rentCar;
-
-public interface ICarGo {
-	//载货方法,应返回载货吨位
-	public abstract double getCarGo();
-	public abstract void setCarGo(double carGoNumber);
-}
--- src/system/rentCar/IManned.java
+++
@@ -1,8 +0,0 @@
-package system.rentCar;
-
-public interface IManned {
-	//载人方法,应返回载人数量
-	public abstract int getManned();
-	public abstract void setManned(int mannedNumber);
-
-}
