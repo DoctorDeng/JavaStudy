8a3eea309a436d6513580cce85f341b615d0bab4
DoctorDeng <1412145477@qq.com>
2018-06-28 12:17:39 +0800

    练习 Jedis Cluster 故障转移

--- src/db/nosql/redis/jedis/JedisClusterTest.java
+++ src/db/nosql/redis/jedis/JedisClusterTest.java
@@ -1,12 +1,15 @@
 package db.nosql.redis.jedis;
 
 import lombok.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 import redis.clients.jedis.*;
 
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author <a href="http://doctordeng.vip/">Doctor邓</a>
@@ -15,6 +18,7 @@ import java.util.Set;
  * @since 2018/6/14 21:05
  */
 public class JedisClusterTest {
+    private static final Logger log = LoggerFactory.getLogger(JedisClusterTest.class);
     private static JedisCluster cluster;
     static {
         Set<HostAndPort> nodes = new HashSet<>();
@@ -41,6 +45,7 @@ public class JedisClusterTest {
      * 练习 Redis Cluster 多节点命令
      */
     private static void multiClusterNodeCommandTest() {
+        // 获取所有节点
         Map<String, JedisPool> jedisPoolMap = cluster.getClusterNodes();
         for (Map.Entry<String, JedisPool> entry : jedisPoolMap.entrySet()) {
             // 获取每个节点的 Jedis 连接
@@ -65,8 +70,29 @@ public class JedisClusterTest {
         }
     }
 
+    /**
+     * Redis Cluster 故障转移测试
+     */
+    private static void failoverTest() {
+        int i = 0;
+        while (true) {
+            String key = "key" + i;
+            String value = "value" + i;
+            try {
+                cluster.set(key, value);
+                log.info(cluster.get(key));
+                TimeUnit.SECONDS.sleep(1);
+            } catch (Exception e) {
+                //e.printStackTrace();
+                log.error(e.getMessage(), e);
+            }
+            i++;
+        }
+    }
+
     public static void main(String[] args) {
         //simpleTest();
-        multiClusterNodeCommandTest();
+        //multiClusterNodeCommandTest();
+        failoverTest();
     }
 }
