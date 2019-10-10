588af20ab24d6f5422b3049fa291d50d2a82f651
DoctorDeng <1412145477@qq.com>
2019-10-10 15:02:19 +0800

    [Update] 优化字符串格式化练习代码注释

--- src/string/StringFormatTest.java
+++ src/string/StringFormatTest.java
@@ -2,7 +2,15 @@ package string;
 
 import java.text.MessageFormat;
 
+
 public class StringFormatTest {
+	/**
+	 * 格式化字符串
+	 * @param template 需要格式字话的符串, 格式见 {@link java.util.Formatter}
+	 * @param params 格式化字符串需要的参数, 格式见 {@link java.util.Formatter}
+	 * @see java.lang.String#format(String, Object...)
+	 * @return 格式化后的字符串
+	 */
 	public static String stringFormat(String template, Object... params) {
 		if (template == null) {
 			return null;
@@ -13,7 +21,11 @@ public class StringFormatTest {
 		}
 	}
 	/**
-	 * 性能优于 {@link StringFormatTest#stringFormat(String, Object...)}
+	 * 格式化字符串, 性能优于 {@link StringFormatTest#stringFormat(String, Object...)}
+	 * @param template 需要格式字话的符串
+	 * @param params   格式化字符串需要的参数
+	 * @see java.text.MessageFormat#format(String, Object...)
+	 * @return 格式化后的字符串
 	 */
 	public static String messageFormat(String template, Object... params) {
 		if (template == null) {
