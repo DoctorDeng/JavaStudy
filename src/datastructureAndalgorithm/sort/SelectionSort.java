0be01fcad30fbd9c3f7276192acb86331fb79a1f
邓华杰 <vip.doctordeng@gmail.com>
2021-04-08 17:28:46 +0800

    [Add] 选择排序练习

---
+++ src/datastructureAndalgorithm/sort/SelectionSort.java
@@ -0,0 +1,43 @@
+package datastructureAndalgorithm.sort;
+
+/**
+ * 选择排序.
+ *
+ * @author DoctorDeng
+ * @version 1.0.0
+ * @date 2021/4/8 16:44
+ */
+public class SelectionSort extends BasicSort {
+    @Override
+    public void sort(int[] array) {
+        checkArguments(array);
+        selectionSort(array, 0, array.length - 1);
+    }
+
+    public static void selectionSort(int[] array, int start, int end) {
+        checkArguments(array, start, end);
+
+        for (int i = start; i < end; i++) {
+            int minIndex = i;
+            int min = array[minIndex];
+            for (int j = i + 1; j <= end; j++) {
+                int value = array[j];
+                if (value < min) {
+                    min = value;
+                    minIndex = j;
+                }
+            }
+            swap(array, minIndex, i);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "选择排序";
+    }
+
+    public static void main(String[] args) {
+        new SelectionSort().simpleTest();
+        new SelectionSort().largeDataTest();
+    }
+}
