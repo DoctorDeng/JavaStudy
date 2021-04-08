62ba416a8c73f46f3aedc4421fa2689b0276344b
邓华杰 <vip.doctordeng@gmail.com>
2021-04-08 15:45:46 +0800

    [Improve] 插入排序注释优化

--- src/datastructureAndalgorithm/sort/InsertionSort.java
+++ src/datastructureAndalgorithm/sort/InsertionSort.java
@@ -24,18 +24,23 @@ public class InsertionSort extends BasicSort {
     public static void insertionSort(int[] array, int arrayLength) {
         checkArguments(array, arrayLength);
 
+        // 将未排序元素依次插入到已排序序列
         for (int i = 1; i < arrayLength; i++) {
             int noSortValue = array[i];
-
+            // 将未排序元素与已排序元素逐个比较
+            // 备注：从右往左遍历，保证稳定性
             int j = i - 1;
-            // 将未排序元素插入到已排序集合中
             for (; j >= 0; j--) {
+                // 当未排序元素比已排序元素小，已排序元素右移
                 if (array[j] > noSortValue) {
                     array[j + 1] = array[j];
-                } else {
+                }
+                // 当未排序元素比已排序元素大，结束遍历
+                else {
                     break;
                 }
             }
+            // 插入未排序元素到正确位置
             array[j + 1] = noSortValue;
         }
     }
