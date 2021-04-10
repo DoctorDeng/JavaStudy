5b2d0d13d97a90ac390f0a7125b621772378deb3
邓华杰 <vip.doctordeng@gmail.com>
2021-04-10 15:26:17 +0800

    refactor(sort): 优化归并排序注释，调整方法名称

--- src/datastructureAndalgorithm/sort/MergeSort.java
+++ src/datastructureAndalgorithm/sort/MergeSort.java
@@ -47,23 +47,25 @@ public class MergeSort extends BasicSort {
      */
     private void mergeSortByLoop(int[] array, int start, int end) {
         int length = end - start + 1;
+        int[] temp = new int[length];
         for (int range = 1; range < length; range = range + range) {
             for (int _start = start; _start <= end; _start += range + range) {
                 int _end = _start + range + range - 1;
                 if (_end > end) {
                     _end = end;
                 }
-                int mid = _start + range -1;
-                if (mid > end) {
-                    mid = end;
+                int _mid = _start + range -1;
+                if (_mid > end) {
+                    _mid = end;
                 }
-                merge(array, _start, mid, _end);
+                mergeNotCreateTempArray(array, temp, _start, _mid, _end);
+                //merge(array, _start, _mid, _end);
             }
         }
     }
 
     /**
-     * 将数组下标 start ~ mid 和 mid + 1 ~ end 两范围内的数据进行归并排序，每次归并都会创建临时数组.
+     * 将数组下标 start ~ mid 和 mid + 1 ~ end 两范围内的数据进行归并操作，每次归并都会创建临时数组.
      * @param array 待归并数组
      * @param start 起始下标
      * @param mid   中间下标
@@ -94,19 +96,19 @@ public class MergeSort extends BasicSort {
     }
 
     /**
-     * 将数组下标 start ~ mid 和 mid + 1 ~ end 两范围内的数据进行归并排序，基于原地归并实现.
+     * 将数组下标 start ~ mid 和 mid + 1 ~ end 两范围内的数据进行归并操作，使用传入的临时数组进行归并操作.
      * @param array 待归并数组
      * @param temp  临时数组, 大小应与 array 一样
      * @param start 起始下标
      * @param mid   中间下标
      * @param end   结尾下标
      */
-    private void mergeInPlace(int[] array, int[] temp, int start, int mid, int end) {
+    private void mergeNotCreateTempArray(int[] array, int[] temp, int start, int mid, int end) {
         int leftCursor = start;
         int rightCursor = mid + 1;
         // 1. 复制待排序数据到临时数组中
         System.arraycopy(array, start, temp, start, end - start + 1);
-        // 2. 进行归并排序并直接将数据复制回原数组
+        // 2. 进行归并操作并直接将数据复制回原数组
         for (int i = start; i <= end; i++) {
             if (leftCursor > mid) {
                 array[i] = temp[rightCursor++];
