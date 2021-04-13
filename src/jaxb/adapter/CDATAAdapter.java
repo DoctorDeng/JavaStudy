58bd22acdfd56f1afa10f4ed385d499253c92b7d
邓华杰 <vip.doctordeng@gmail.com>
2021-04-13 12:17:49 +0800

    refactor(jaxb): CDATAAdapter 注释调整, 移除 StringBuilder

--- src/jaxb/adapter/CDATAAdapter.java
+++ src/jaxb/adapter/CDATAAdapter.java
@@ -3,7 +3,7 @@ package jaxb.adapter;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 /**
- * XmlAdapter 类, 修饰类字段, 达到自动添加 CDATA 标记的目标
+ * XmlAdapter 类, 修饰类字段, 达到自动添加 CDATA 标记的目的
  * @author denghuajie
  * @since 2018年12月22日
  */
@@ -15,6 +15,6 @@ public class CDATAAdapter extends XmlAdapter<String, String> {
 
 	@Override
 	public String marshal(String value) throws Exception {
-		return new StringBuilder("<![CDATA[").append(value).append("]]>").toString();
+		return "<![CDATA[" + value + "]]>";
 	}
 }
