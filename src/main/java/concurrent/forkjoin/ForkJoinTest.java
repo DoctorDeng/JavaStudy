526e47454f1a0d1d29a6e940ec8aa6773dc6d4a7
邓华杰 <vip.doctordeng@gmail.com>
2021-08-13 20:28:09 +0800

    refactor(cocurrent): 优化 ForkJoinTest 历史练习中不合理之处

--- src/main/java/concurrent/forkjoin/ForkJoinTest.java
+++ src/main/java/concurrent/forkjoin/ForkJoinTest.java
@@ -1,23 +1,26 @@
 package concurrent.forkjoin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 import java.util.concurrent.RecursiveTask;
 
 /**
- * 练习 JDK 1.7 新增的 Fork/Join 框架
+ * 练习 JDK 1.7 新增的 Fork/Join 框架.
  *
  * @author <a href="http://doctordeng.vip/">DoctorDeng</a>
  * @since 2018年11月23日
  */
 public class ForkJoinTest {
-    private static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(CountTask.class);
+    private static final Logger log = LoggerFactory.getLogger(CountTask.class);
 
     public static void main(String[] args) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         // 生成一个计算任务，负责计算 1 + 2 + 3 + 4
-        CountTask countTask = new CountTask(1, 4);
+        CountTask countTask = new CountTask(1, 10000);
         // 执行一个任务
         Future<Integer> result = forkJoinPool.submit(countTask);
 
@@ -29,7 +32,7 @@ public class ForkJoinTest {
         try {
             System.out.println(result.get());
         } catch (InterruptedException | ExecutionException e) {
-            //e.printStackTrace();
+            e.printStackTrace();
         }
     }
 
@@ -43,8 +46,8 @@ class CountTask extends RecursiveTask<Integer> {
     private static final long serialVersionUID = -2884067762236351099L;
     // threshold : 门槛、阀值
     private static final int THRESHOLD = 2;
-    private int start;
-    private int end;
+    private final int start;
+    private final int end;
 
     public CountTask(int start, int end) {
         this.start = start;
@@ -60,9 +63,7 @@ class CountTask extends RecursiveTask<Integer> {
             for (int i = start; i <= end; i++) {
                 //sum += i;
                 sum = sum + i;
-                throw new RuntimeException("aaa");
             }
-
         } else {
             // 如果任务大于阀值, 就分裂成两个子任务计算
             int middle = (start + end) / 2;
@@ -74,12 +75,12 @@ class CountTask extends RecursiveTask<Integer> {
             rightTask.fork();
 
             // 等待子任务执行完成并得到结果
-            int leftResult = leftTask.join();
             int rightResult = rightTask.join();
+            int leftResult = leftTask.join();
             // 合并子任务
             sum = leftResult + rightResult;
         }
 
-        return Integer.valueOf(sum);
+        return sum;
     }
 }
