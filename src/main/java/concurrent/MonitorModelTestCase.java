6dc52e3d34823e171d5467ae277ea32f70b55cb9
邓华杰 <vip.doctordeng@gmail.com>
2021-09-02 10:09:17 +0800

    refactor(concurrent): 优化 MonitorModelTestCase 练习.

--- src/main/java/concurrent/MonitorModelTestCase.java
+++ src/main/java/concurrent/MonitorModelTestCase.java
@@ -1,17 +1,23 @@
 package concurrent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
 /**
- * Java 管程模型验证, 验证 Java 中的管程模型属于下面哪中模型：
- * <p>
- *     1. MESA  模型：线程 B 唤醒线程 A. A 继续等待, B 依旧执行，直到完成.
- *     2. Hasen 模型：线程 B 唤醒线程 A. 唤醒操为最后才可以执行的操作.
- *     3. Hoare 模型：线程 B 唤醒线程 A. A 开始执行, B 进行等待.
- * </p>
- * 测试结果：线程 B 唤醒线程 A 后，B 直到执行完毕线程 A 才能够执行因此 Java 遵守 MESA 模型.
+ * Java 管程模型验证, 验证 Java 中的管程模型属于下面哪种模型：
+ * <p>   1. MESA  模型：线程 B 唤醒线程 A. A 继续等待, B 依旧执行，直到完成.
+ * <p>   2. Hasen 模型：线程 B 唤醒线程 A. 唤醒操为最后才可以执行的操作.
+ * <p>   3. Hoare 模型：线程 B 唤醒线程 A. A 开始执行, B 进行等待.
+ * <p>测试结果：线程 B 唤醒线程 A 后，B 直到执行完毕线程 A 才能够执行因此 Java 遵守 MESA 模型.
+ *
  * @version 1.0.0
  * @date 2021/7/16 18:55
  */
 public class MonitorModelTestCase {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MonitorModelTestCase.class);
 
     /**
      * 打印结果：
@@ -29,7 +35,7 @@ public class MonitorModelTestCase {
 
         Runnable a = () -> {
             while (true) synchronized (lock) {
-                System.out.println("this is Runnable A");
+                LOGGER.info("this is Runnable A");
                 try {
                     lock.notifyAll();
                     lock.wait();
@@ -43,8 +49,8 @@ public class MonitorModelTestCase {
                 lock.notifyAll();
                 try {
                     for (int i = 0; i < 2; i++) {
-                        Thread.sleep(1000);
-                        System.out.println("this is Runnable B");
+                        TimeUnit.SECONDS.sleep(1);
+                        LOGGER.info("this is Runnable B");
                     }
                     lock.wait();
                 } catch (InterruptedException e) {
