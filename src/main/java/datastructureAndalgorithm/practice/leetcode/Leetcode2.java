ae1c25af3d567a1d2f9dd155abe505839f7af812
denghuajie <vip.doctordeng@gmail.com>
2022-12-25 14:53:51 +0800

    refactor(leetcode): 代码优化.

--- src/main/java/datastructureAndalgorithm/practice/leetcode/Leetcode2.java
+++ src/main/java/datastructureAndalgorithm/practice/leetcode/Leetcode2.java
@@ -7,7 +7,7 @@ package datastructureAndalgorithm.practice.leetcode;
  * 请你将两个数相加，并以相同形式返回一个表示和的链表.
  * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头.
  * </pre>
- *
+ * <p>
  * 说明见 <a href=https://leetcode.cn/problems/add-two-numbers>Leetcode 2: 两数相加</a>
  *
  * @author DoctorDeng
@@ -15,43 +15,49 @@ package datastructureAndalgorithm.practice.leetcode;
  * @date 2022/12/24 20:27
  */
 public class Leetcode2 {
-    class ListNode {
-          int val;
-          ListNode next;
-          ListNode() {}
-          ListNode(int val) { this.val = val; }
-          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     }
+    static class ListNode {
+        int val;
+        ListNode next;
 
-
-    class Solution {
-
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            ListNode l1Cursor = l1;
-            ListNode l2Cursor = l2;
-            ListNode head = new ListNode();
-            ListNode cursor = head;
-
-            int remainder = 0;
-            while (l1Cursor != null || l2Cursor != null) {
-                int val = remainder;
-                if (l1Cursor != null) {
-                    val = l1Cursor.val + val;
-                    l1Cursor = l1Cursor.next;
-                }
-                if (l2Cursor != null) {
-                    val = l2Cursor.val + val;
-                    l2Cursor = l2Cursor.next;
-                }
-                cursor.next = new ListNode(val % 10);
-                remainder = val / 10;
-                cursor = cursor.next;
-            }
-            if (remainder != 0) {
-                cursor.next = new ListNode(remainder);
-            }
-            return head.next;
+        ListNode() {
         }
 
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l1Cursor = l1;
+        ListNode l2Cursor = l2;
+        ListNode head = new ListNode();
+        ListNode cursor = head;
+
+        // 进位值, 例如: 5 + 6 = 1 要向高位进 1.
+        int remainder = 0;
+        while (l1Cursor != null || l2Cursor != null) {
+            int val = remainder;
+            if (l1Cursor != null) {
+                val = l1Cursor.val + val;
+                l1Cursor = l1Cursor.next;
+            }
+            if (l2Cursor != null) {
+                val = l2Cursor.val + val;
+                l2Cursor = l2Cursor.next;
+            }
+            cursor.next = new ListNode(val % 10);
+            remainder = val / 10;
+            cursor = cursor.next;
+        }
+        if (remainder != 0) {
+            cursor.next = new ListNode(remainder);
+        }
+        return head.next;
+    }
+
 }
